bc0f87542e94c998db4316f1acb5b90abec65412
tariktarik <tarik.bajric@gmail.com>
2021-03-15 21:16:59 +0100

    Functions with arguments

---
+++ ex02/src/index.js
@@ -0,0 +1,11 @@
+// Write code below this line
+
+function functionPrintInfo(name, age) {
+    console.log(name, age);
+}
+
+functionPrintInfo("Jhon", 23);
+
+// Write code above this line 
+
+module.exports = functionPrintInfo;
